ab46210bc11b2526d3b67482efcf548ad447eda7
Jingru Zhao <jingru01@ucm.es>
2021-02-23 10:33:33 +0100

    23FEB

--- prueba/src/FlyBehaviour/IFlyBehaviour.java
+++
@@ -1,5 +0,0 @@
-package FlyBehaviour;
-
-public interface IFlyBehaviour {
-	public void fly();
-}
--- prueba/src/QuackBehaviour/IQuackBehaviour.java
+++
@@ -1,6 +0,0 @@
-package QuackBehaviour;
-
-public interface IQuackBehaviour {
-	public void quack();
-
-}
